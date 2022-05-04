207a0fbe588b2a6aec89d55b9d4c52c9939b1d67
paul0817 <100745610+paul0817@users.noreply.github.com>
2022-05-04 09:22:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name = 'mycalc0817',
+  version = '1.0.0.',
+  description = 'oss my calculator',
+  author = 'paul0817',
+  author_email = 'pau10817@naver.com',
+  url = 'https://github.com/paul0817/mycalc0817/',
+  install_requires = ['pytest'], 
+  long_description = 'mycalc0817',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+    "programming Language :: Pytion :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
